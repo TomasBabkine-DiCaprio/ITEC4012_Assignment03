4158b40c2a32e216e3115e09ecbbceb3779a7933
TomasBabkineDiCaprio <babkinedicapriotomas@gmail.com>
2021-11-30 21:35:19 -0500

    Added homepage and API link

---
+++ src/components/GroceryItem/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css';
+import { Link } from 'react-router-dom';
+
+export const GroceryItem = (props) => {
+
+    const { id, name, price, img, weight, category, expires } = props;
+
+    return (
+        <div className="groceryItem">
+            <img className="grocery-img" src={img} alt={name} />
+            <Link to={`/item/${id}`}>
+                <h1 className="grocery-name">{name}</h1>
+            </Link>
+        </div>
+    )
+}
---
+++ src/components/pages/Home-Page/index.jsx
@@ -0,0 +1,54 @@
+import "./styles.css";
+import { useEffect, useState } from 'react';
+
+// Router
+import { useHistory } from "react-router";
+
+export const HomePage = () => {
+
+    // Groceries state
+    const [groceries, setGroceries] = useState([]);
+
+    // Loading state (used to display a loading message while awaiting a response from the API)
+    const [loading, setLoading] = useState([]);
+
+    useEffect(
+        () => {
+            getGroceries();
+        }, []
+    );
+
+    // Get groceries from the API
+    const getGroceries = async() => {
+        try {
+            const response = await fetch('https://firestore.googleapis.com/v1/projects/itec4012-a03/databases/(default)/documents/groceries');
+            const data = await response.json();
+
+            const formattedData = data.documents.map( (item) => {
+                return item.fields;
+            })
+
+            setGroceries(formattedData);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    }
+
+    // Router history
+    const history = useHistory();
+    console.log(groceries);
+    return (
+        <div className="groceries-page">
+            <h1>Grocery List</h1>
+            <div className="groceries-container">
+                {
+                    groceries.map( (item) => {
+                        
+                    })
+                }
+            </div>
+        </div>
+    )
+}
